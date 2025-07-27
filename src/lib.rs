dc15dc49cdbf9347c8ac3ab3ea87e1fc4575e19a
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2025-07-27 15:52:48 -0300

    mostly make things work

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
-use std::sync::Arc;
 use arc_swap::ArcSwap;
+use std::sync::Arc;
 
 pub enum AnyCow<'a, T>
-where T: 'a + ToOwned
+where
+    T: 'a + ToOwned,
 {
     Borrowed(&'a T),
     Boxed(Box<T>),
@@ -11,7 +12,8 @@ where T: 'a + ToOwned
 }
 
 impl<'a, T> AnyCow<'a, T>
-where T: 'a + ToOwned
+where
+    T: 'a + ToOwned<Owned = T>,
 {
     pub fn updatable(value: T) -> Self {
         AnyCow::Updatable(ArcSwap::from(Arc::new(value)))
@@ -22,40 +24,62 @@ where T: 'a + ToOwned
             AnyCow::Borrowed(value) => value.to_owned(),
             AnyCow::Boxed(value) => *value,
             AnyCow::Shared(value) => (*value).to_owned(),
-            AnyCow::Updatable(value) => {
-                value.load().as_ref().to_owned()
-            }
+            AnyCow::Updatable(value) => value.load().as_ref().to_owned(),
         }
     }
 
-    pub fn replace(&self, new_val: T) {
+    pub fn borrow(&self) -> &T {
+        match self {
+            AnyCow::Borrowed(value) => value,
+            AnyCow::Boxed(value) => &*value,
+            AnyCow::Shared(value) => &*value,
+            AnyCow::Updatable(value) => {
+
+            },
+        }
+    }
+
+    pub fn try_replace(&self, new_val: T) -> Result<(), ()> {
         if let AnyCow::Updatable(a) = self {
             a.store(Arc::new(new_val));
+            Ok(())
+        } else {
+            Err(())
         }
     }
 
-    pub fn borrow(&self) -> AnyBorrow<'a, T>; // like your StyleRef; Deref<Target=T>
-    pub fn try_replace(&self, new_val: T) -> Result<(), NotUpdatable>;
-    pub fn try_into_owned(self) -> Result<T, Self>;
-    pub fn to_arc(&self) -> std::sync::Arc<T>;
-
-    // If you want true CoW semantics:
-    pub fn to_mut(&mut self) -> &mut T where T: Clone;
+    pub fn to_arc(&self) -> Arc<T> {
+        match self {
+            AnyCow::Borrowed(value) => Arc::new((*value).to_owned()),
+            AnyCow::Boxed(value) => Arc::new(value.as_ref().to_owned()),
+            AnyCow::Shared(value) => value.clone(),
+            AnyCow::Updatable(value) => value.load().to_owned(),
+        }
+    }
 }
 
-impl<T> From<T> for AnyCow<'_, T> {
+impl<T> From<T> for AnyCow<'_, T>
+where
+    T: ToOwned,
+{
     fn from(value: T) -> Self {
         AnyCow::Boxed(Box::new(value))
     }
 }
 
-impl<'a, T> From<&'a T> for AnyCow<'a, T> {
+impl<'a, T> From<&'a T> for AnyCow<'a, T>
+where
+    T: 'a + ToOwned,
+{
     fn from(value: &'a T) -> Self {
         AnyCow::Borrowed(value)
     }
 }
 
-impl<T> From<Arc<T>> for AnyCow<'_, T> {
+impl<T> From<Arc<T>> for AnyCow<'_, T>
+where
+    T: ToOwned,
+{
     fn from(value: Arc<T>) -> Self {
         AnyCow::Shared(value)
     }
